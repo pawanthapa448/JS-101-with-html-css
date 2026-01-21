85a24333c5934c0317f827db42713c79bc49c7f4
Saugat Bagale <MontessoriVisualization@users.noreply.github.com>
2026-01-21 13:47:43 +0545

    functipon revision

---
+++ basics0/hi.js
@@ -0,0 +1,39 @@
+function add(num1,num2=8) {
+    console.log(num1+num2);
+}
+add(5,12);
+add(7);
+add(10);
+
+function addAll(...bbrd) {
+    console.log(bbrd);
+
+}
+
+
+addAll(1,2,3,4,5,6,7,8,9,10);
+addAll(10,20,30);
+
+let num1 = 20;
+let num2 = 10;
+let num3;
+function operatingNUM() {  
+  num2=55
+    console.log(num1)
+   function operating() {  
+   const num3 = 15;
+       console.log(num2);
+    addAll(1,2,3,4,5,6,7,8,9);
+
+     console.log(num3);
+     }
+     
+     operating();
+     console.log(num3);
+
+
+}
+console.log(num2);
+operatingNUM();
+
+
